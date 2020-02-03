ce9841a9f376d442368c0109fef3ab20c811fe89
keita2630401 <yms1991@icloud.com>
2020-02-03 20:17:31 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -34,12 +34,15 @@ $(function(){
               ${message.content}
             </p>
           </div>
-        </div>`
+         </div>`
       }else if (message.image){
         var html=`<div class="message"data-message-id= ${message.id}>
           <div class="upper-message">
             <div class="upper-message__user-name">
               ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
             </div>
           </div>
           <div class="lower-message">
